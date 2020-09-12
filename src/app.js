9ffe6e1d64b963a0a8551b89393deaf2addd4e89
Nishmita Shetty <Nishmita.Shetty@lntinfotech.com>
2020-09-13 03:15:53 +0530

    commit

--- src/app.js
+++ src/app.js
@@ -1,31 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import {Provider} from 'react-redux';
+import { Provider } from 'react-redux';
 import AppRouter from './routers/AppRouter';
-import configureStore from './store/configureStore';
+import configureStore from './store/configurestore';
 import { addExpense } from './actions/expenses';
 import { setTextFilter } from './actions/filters';
 import getVisibleExpenses from './selectors/expenses';
 import 'normalize.css/normalize.css';
 import './styles/styles.scss';
+import 'react-dates/lib/css/_datepicker.css';
 
 const store = configureStore();
 
-store.dispatch(addExpense({ description: 'Water bill' ,amount: 100}));
-store.dispatch(addExpense({ description: 'Gas bill' ,amount: 300 }));
-console.log("testing 123");
-
-<h2>dsd</h2>
-const state = store.getState();
-const visibleExpenses = getVisibleExpenses(state.expenses, state.filters);
-console.log(visibleExpenses);
-const jsx=(
-    <Provider store={store}>
+const jsx = (
+  <Provider store={store}>
     <AppRouter />
-    </Provider>
-    
-  )
+  </Provider>
+);
+
 ReactDOM.render(jsx, document.getElementById('app'));
+
 
 
 
