425fd63b9206580b6d185cafc907edf8251d0d3a
Nishmita Shetty <Nishmita.Shetty@lntinfotech.com>
2020-09-13 01:13:03 +0530

    setup build and server

--- server/server.js
+++ server/server.js
@@ -1,22 +1,21 @@
-const path = require('path');
+const path = require('path'); //node js way of importing is by using require, so we're importing path and express
 const express = require('express');
-const app = express();
-const publicPath = path.join(__dirname, '..', 'public');
-app.use(express.static(publicPath));
+const app = express(); //assigned the imported express to const app
+const publicPath = path.join(__dirname, '..', 'public'); //path.join links our server to the given path
+
 //public app serves all assets in public directory
+app.use(express.static(publicPath)); //express.static is a predefined method where we're passing public path
 
-
-
+const port = process.env.PORT || 3000;
 app.get('*', (req, res) => {
     res.sendFile(path.join(publicPath, 'index.html'));
   });
   
-  app.listen(3000, () => {
-    console.log('Server is up!');
+  app.listen(port, () => {
+    console.log('Server is up!');   //3000=port that express should use
+
   });
   
-
-//3000=port that express should use
 
 
 
